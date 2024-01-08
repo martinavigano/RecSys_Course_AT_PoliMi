1a992f8168ec111e9cc93fe5a97ac514b49b6ea6
Martina Viganò <100607513+martinavigano@users.noreply.github.com>
2024-01-08 09:50:29 +0100

    Update SLIMElasticNetRecommender.py

--- Recommenders/SLIM/SLIMElasticNetRecommender.py
+++ Recommenders/SLIM/SLIMElasticNetRecommender.py
@@ -61,11 +61,11 @@ class SLIMElasticNetRecommender(BaseItemSimilarityMatrixRecommender):
                                 max_iter=100,
                                 tol=1e-4)
 
-        URM_train = check_matrix(self.URM_train, 'csc', dtype=np.float32)
+        URM_train = check_matrix(self.URM_train, 'csc', dtype=float)
 
         n_items = URM_train.shape[1]
 
-        similarity_builder = Incremental_Similarity_Builder(self.n_items, initial_data_block=self.n_items*self.topK, dtype = np.float32)
+        similarity_builder = Incremental_Similarity_Builder(self.n_items, initial_data_block=self.n_items*self.topK, dtype = float)
 
         start_time = time.time()
         start_time_printBatch = start_time
@@ -99,7 +99,7 @@ class SLIMElasticNetRecommender(BaseItemSimilarityMatrixRecommender):
                 nonzero_model_coef_value = nonzero_model_coef_value[relevant_items_partition]
 
             similarity_builder.add_data_lists(row_list_to_add=nonzero_model_coef_index,
-                                              col_list_to_add=np.ones(len(nonzero_model_coef_index), dtype = np.int) * currentItem,
+                                              col_list_to_add=np.ones(len(nonzero_model_coef_index), dtype = int) * currentItem,
                                               data_list_to_add=nonzero_model_coef_value)
 
 
@@ -214,7 +214,7 @@ class MultiThreadSLIM_SLIMElasticNetRecommender(SLIMElasticNetRecommender):
 
         self.workers = workers
 
-        self.URM_train = check_matrix(self.URM_train, 'csc', dtype=np.float32)
+        self.URM_train = check_matrix(self.URM_train, 'csc', dtype=float)
 
         indptr_shm = create_shared_memory(self.URM_train.indptr)
         indices_shm = create_shared_memory(self.URM_train.indices)
@@ -253,5 +253,5 @@ class MultiThreadSLIM_SLIMElasticNetRecommender(SLIMElasticNetRecommender):
         data_shm.unlink()
 
         # generate the sparse weight matrix
-        self.W_sparse = sps.csr_matrix((values, (rows, cols)), shape=(self.n_items, self.n_items), dtype=np.float32)
+        self.W_sparse = sps.csr_matrix((values, (rows, cols)), shape=(self.n_items, self.n_items), dtype=float)
         self.URM_train = self.URM_train.tocsr()
